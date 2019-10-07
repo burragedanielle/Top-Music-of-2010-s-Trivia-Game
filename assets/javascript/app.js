a20e6db7c6628f7655dfd2b463bc2231db8d115d
Danielle Burrage <burragedanielle@gmail.com>
2019-10-06 22:14:22 -0500

    js timer functionality added // question text added // moving to new question functionality still broken

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,136 @@
 
     var questionsAnswers = [
         {
-            question: "blah blah blah question 1",
-            guessOption: ["pick 1", "pick 2"],
-            correct: "3",
-            correctTitle: "Valentina",
-            correctInfo: "Blah blah paragraph 1"
+            question: "how long was bad guy by Billie Eilish #1 on the Billboard charts?",
+            guessOption: ["three weeks", "one week", "two weeks", "six weeks"],
+            correct: "1",
+            correctTitle: "one week!",
+            correctInfo: "Billie Eilish earned her first number one single in August 2019, after a record-breaking nine-week wait in the runner up spot."
         },
         {
-            question: "blah blah blah question 2",
-            guessOption: ["pick 6", "pick 7"],
-            correct: "2",
-            correctTitle: "Yuki", 
-            correctInfo: "Blah blah paragraph 2"
+            question: "Which rapper with nearly 17,000,000 monthly listeners on Spotify released a new album in late 2019, titled KIRK?",
+            guessOption: ["2 Chainz", "Pusha T", "Isaiah Rashad", "DaBaby"],
+            correct: "3",
+            correctTitle: "DaBaby", 
+            correctInfo: "Rising rap star DaBaby released album KIRK in September 2019, reaching No. 1 Album on the Billboard Charts"
 
         },
         {
-            question: "blah blah blah question 3 ",
-            guessOption: ["pick 30", "pick 35"],
-            correct: "1",
-            correctTitle: "Delilah",
-            correctInfo: "Blah blah paragraph 3"
+            question: "What is the most globally streamed song of 2019 on Spotify as of October 1, 2019?",
+            guessOption: ["Señorita - Camila Cabello and Shawn Mendes", "Great White Buffalo - Ted Nugent", "Shape of You - Ed Sheeran", "Sunflower - Post Malone"],
+            correct: "0",
+            correctTitle: "Señorita by Camila Cabello and Shawn Mendes",
+            correctInfo: ""
         },
         {
-            question: "blah blah blah question 4",
-            guessOption: ["pick 1", "pick 2"],
-            correct: "4", 
-            correctTitle: "Goober",
+            question: "Who has the most songs on Spotify’s USA Top 200 as of Oct. 1,  2019?",
+            guessOption: ["Ariana Grande", "Taylor Swift", "Ed Sheeran", "Post Malone"],
+            correct: "3", 
+            correctTitle: "Post Malone",
             correctInfo: "Blah blah paragraph 4"
-        }
+        },
+        {
+            question: "Which breakout Houston singer and rapper ranks 63rd on Spotify’s top listened to artists as of Oct. 1, 2019?",
+            guessOption: ["SZA", "Princess Nokia", "Lizzo", "Miley Cyrus"],
+            correct: "2", 
+            correctTitle: "Lizzo",
+            correctInfo: "Blah blah paragraph 4"
+        },
+        {
+            question: "Where does Taylor Swift rank on Spotify’s most listened to artists charts as of Oct. 1, 2019",
+            guessOption: ["14th", "1st", "2nd", "7th"],
+            correct: "0", 
+            correctTitle: "14th",
+            correctInfo: "Blah blah paragraph 4"
+        },
+        {
+            question: "Which rapper defined the 'Hot Girl Summer' with 2019 releases such as Cash Shit (feat. DaBaby) and Simon Says (feat. Juicy J)?",
+            guessOption: ["Lizzo", "Kash Doll", "Megan Thee Stallion", "Nicki Minaj"],
+            correct: "2", 
+            correctTitle: "Megan Thee Stallion",
+            correctInfo: "Blah blah paragraph 4"
+        },
+        {
+            question: "Who is the number one streamed artist on Spotify as of Oct. 1, 2019?",
+            guessOption: ["Justin Bieber", "Ariana Grande", "Shawn Mendez", "Ed Sheeran"],
+            correct: "3", 
+            correctTitle: "Ed Sheeran",
+            correctInfo: "Blah blah paragraph 4"
+        },
+        {
+            question: "Which cult-favorite indie-rock band released their long-awaited 4th studio album in May 2019?",
+            guessOption: ["alt-J", "Vampire Weekend", "Florence + Machine", "HAIM"],
+            correct: "1", 
+            correctTitle: "Vampire Weekend",
+            correctInfo: "Blah blah paragraph 4"
+        },
+        {
+            question: "What 2019 hit was #1 on the Billboard Hot 100 for a record-breaking 19 consecutive weeks?",
+            guessOption: ["7 Rings - Ariana Grande", "Old Town Road - Lil Nas X", "Sunflower - Post Malone", "High Hopes - Panic! At The Disco"],
+            correct: "1", 
+            correctTitle: "Old Town Road - Lil Nas X",
+            correctInfo: "Blah blah paragraph 4"
+        },
+        {
+            question: "Where does Beyonce rank on Spotify’s Most Streamed Artists chart as of Oct.1, 2019?",
+            guessOption: ["#3 Most Streamed", "#63 Most Streamed", "#42 Most Streamed", "#10 Most Streamed"],
+            correct: "2", 
+            correctTitle: "#42 Most Streamed",
+            correctInfo: "Blah blah paragraph 4"
+        },
+        {
+            question: "Which Charlie’s Angels trio member released a new album in Aug 2019 featuring a Sublime cover?", 
+            guessOption: ["Miley Cyrus", "Lana Del Rey", "SZA", "Camila Cabelo"],
+            correct: "1", 
+            correctTitle: "Lana Del Rey with Doin Time",
+            correctInfo: "Blah blah paragraph 4"
+        },
+        {
+            question: "Which platinum-selling singer-songwriter released fourth studio album LOVE + FEAR with hit electronic single “Baby feat. Clean Bandit", 
+            guessOption: ["MARINA (formally Marina and The Diamonds)", "Britney Spears", "Foxes", "Ellie Goulding"],
+            correct: "0", 
+            correctTitle: "MARINA",
+            correctInfo: "Blah blah paragraph 4"
+        },
     ];
 
     // ------------------ VARIABLES ---------------------------------
-
+    
+    var myTimer = null;
+    var myTimeout = null;
     var wins = 0;
     var answered = false; 
     var index = 0; 
-    var time = 10;
+    var time = 15;
+    var clockRunning = false;
 
     function timerStart(){
-        time--;
+        if (!clockRunning) {
+            myTimer = setInterval(count, 1000);
+            clockRunning = true;
+        }
+    }
+
+    function count(){
+        time--
+        if (time === 0) {
+            stop();
+        }
+        
         $("#timer").html(time);
             if(time === 0){
-                console.log("Sorry, no more time left!")
-                clearInterval(timerStart);
+                stop();
                 $("#timer").hide();
-                //resetRound
+                $("#game-contents").html("<h4> Sorry, no more time left! </h4>");
+                
         }   else if (time <= 5) {
                 $("#timer").html("<h4> You have " + time + " seconds left </h4>");
             } 
     };
 
-    function timerStop(){
-        clearInterval(timerStart);
+    function stop(){
+        clearInterval(myTimer);
+        clockRunning = false;
     }
 
     function hideIntro() {
@@ -76,23 +154,23 @@
     //this function starts the game
 
     function startGame(){
+        displayQA();
         $("#display-question").show();
         $("#display-options").show();
         $("#timer").show();
-        displayQA();
-        setInterval(timerStart, 1000); 
     };
 
     //this function occurs when user clicks guess options 
 
     $("#display-options").on("click", ".guess-options", function (event){
+        stop();
+        $("#timer").html("");
         var id = $(this).attr("id");
         if (id === correct){
-            alert("you are right!");
             correctAnswer();
-            clearInterval(timerStart);
+            clearInterval(myTimer);
         } else {
-            clearInterval(timerStart);
+            clearInterval(myTimer);
             incorrectAnswer();
         }
     })
@@ -102,51 +180,54 @@
     $("#start-button").click(function () {
         hideIntro();
         startGame();
+        timerStart();
     });
 
 function correctAnswer(){
-    console.log(index);
-    console.log(timer);
-    $("#display-question").hide();
-    $("#display-options").hide();
-    $("#timer").hide();
-    
+
+    $("#display-question").html("");
+    $("#display-options").html("");
     $("#game-content").append("<div class= correct>").html("<h2>You're right!</h2>");
 
         for (var i = 0; i < questionsAnswers.length; i++) {
             var correctTitle = questionsAnswers[index].correctTitle;
             var correctInfo = questionsAnswers[index].correctInfo;
         }
+
     $("#game-content").append("<h4>" + correctTitle + "</h4>");
     $("#game-content").append("<h4>" + correctInfo + "</h4>");
 
-    setTimeout(function(){
-        $("#game-content").hide();
-        startGame();
 
-    }, 3000);
+    myTimeout = setTimeout(function(){
+        $("#game-content").html("");
+        
+    }, 10000);
 
     index++
     wins++
-    
-    // resetGame();
 };
 
 function incorrectAnswer(){
-    resetGame();
+    $("#display-question").html("");
+    $("#display-options").html("");
+    $("#timer").html("");
+    $("#game-content").append("<div class= correct>").html("<h2>Nice Try!</h2>");
+
+    for (var i = 0; i < questionsAnswers.length; i++) {
+        var correctTitle = questionsAnswers[index].correctTitle;
+        var correctInfo = questionsAnswers[index].correctInfo;
+    }
+
+    $("#game-content").append("<h4>" + correctTitle + "</h4>");
+    $("#game-content").append("<h4>" + correctInfo + "</h4>");
+
+    myTimeout = setTimeout(function(){
+        $("#game-content").html("");
+        
+    }, 3000);
+
     console.log(index);
     index++
-}
-
-function resetGame(){
-    $("#display-question").show();
-    $("#display-options").show();
-    if(index < questionsAnswers.length){
-        setTimeout(function (){
-            displayQA();
-
-        }, 1000)
-    }
 }
 
 
